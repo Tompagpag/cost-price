c81e449b8ffe1d3fbe193b3f7408dbe9615dc106
Tom Paganel <paganel.tom@gmail.com>
2022-03-07 16:11:12 +0100

    add validates for recipe and ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,6 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
+
+  validates :ingredient_name, :amount_for_recipe, :unit_for_recipe,
+            :amount_of_order, :cost_of_order, :unit_of_order, presence: true
 end
