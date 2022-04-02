c7764b5954c948e283f2d9e4ca4cfcc8a46f9201
Tom Paganel <paganel.tom@gmail.com>
2022-04-02 15:24:58 +0200

    get all elements of radio btn

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,4 +1,15 @@
-let checkbox = document.getElementById("ingredient_unit_for_recipe_gramme");
+let unitForRecipeGramme = document.getElementById("ingredient_unit_for_recipe_gramme");
+let unitForRecipeKilo = document.getElementById("ingredient_unit_for_recipe_kilogramme");
+let unitForRecipeCentL = document.getElementById("ingredient_unit_for_recipe_centilitre");
+let unitForRecipeLitre = document.getElementById("ingredient_unit_for_recipe_litre");
+let unitOfOrderGramme = document.getElementById("ingredient_unit_of_order_gramme");
+let unitOfOrderKilo = document.getElementById("ingredient_unit_of_order_kilogramme");
+let unitOfOrderCentL = document.getElementById("ingredient_unit_of_order_centilitre");
+let unitOfOrderLitre = document.getElementById("ingredient_unit_of_order_litre");
 
-checkbox.disabled = true;
-console.log(checkbox);
+
+unitForRecipeGramme.addEventListener('click', (event) => {
+  unitOfOrderCentL.setAttribute("disabled", "");
+  unitOfOrderLitre.setAttribute("disabled", "");
+})
+console.log(unitForRecipeGramme);
