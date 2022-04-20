9423f79e80b423c169dbf441bcb9960624e4f4d9
Tom Paganel <paganel.tom@gmail.com>
2022-04-20 17:16:38 +0200

    adding ingredients and set cost works

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -7,12 +7,10 @@ class Ingredient < ApplicationRecord
   UNITS = ["Gramme", "Kilogramme", "Centilitre", "Litre"]
 
   def set_cost_for_recipe
-    if self.unit_for_recipe == self.unit_of_order
+    if unit_for_recipe == unit_of_order
       self.cost_for_recipe = cost_of_order.fdiv(amount_of_order) * amount_for_recipe
-    elsif self.unit_for_recipe == UNITS[0] && self.unit_of_order == UNITS[1]
-      self.cost_for_recipe = cost_of_order.fdiv((amount_of_order * 1000)) * amount_for_recipe
     else
-      self.cost_for_recipe = 76.6
+      self.cost_for_recipe = cost_of_order.fdiv((amount_of_order * 1000)) * amount_for_recipe
     end
   end
 end
